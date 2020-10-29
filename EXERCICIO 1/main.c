5d143b6c44628003fc48162897764a11b7329c45
Júlia Nogueira <69528739+juliaNogueiraC@users.noreply.github.com>
2020-10-28 22:20:06 -0300

    PPC4

---
+++ EXERCICIO 1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lista.h"
+
+ int main(int argc, char const *argv[])
+{
+	novaLista = inicia_lista();
+
+	printf("tamanho da lista: %d\n",tamanho(novaLista) );
+
+	
+	return 0;
+
+
+}
---
+++ EXERCICIO 3/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lista.h"
+
+ int main(int argc, char const *argv[])
+{
+	
+	int x = troca();
+
+	return 0;
+}
+
+
+
